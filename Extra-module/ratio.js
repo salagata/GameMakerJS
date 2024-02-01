c7f91362ffaf8eb272914b589207f4a5439b5e22
salagata <122649005+salagata@users.noreply.github.com>
2024-02-01 13:48:22 -0500

    Added criptojs to dependencies

--- Extra-module/ratio.js
+++ Extra-module/ratio.js
@@ -1,2 +1,8 @@
-
+class Ratio {
+    constructor(antecedent,consecuent) {
+        this.antecedent = antecedent;
+        this.consecuent = consecuent;
+        this.ratio = antecedent/consecuent;
+    }
+}
 export {}
